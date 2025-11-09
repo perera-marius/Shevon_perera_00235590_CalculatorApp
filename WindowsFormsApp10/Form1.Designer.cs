916407bf911c93b53231a4974649897d8506a69d
perera-marius <pereramarius2003@gmail.com>
2025-11-09 10:30:26 +0530

    Add project files.

---
+++ WindowsFormsApp10/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsApp10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Num1 = new System.Windows.Forms.TextBox();
+            this.Num2 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Num1
+            // 
+            this.Num1.Location = new System.Drawing.Point(85, 25);
+            this.Num1.MinimumSize = new System.Drawing.Size(100, 100);
+            this.Num1.Name = "Num1";
+            this.Num1.Size = new System.Drawing.Size(200, 100);
+            this.Num1.TabIndex = 0;
+            this.Num1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Num2
+            // 
+            this.Num2.Location = new System.Drawing.Point(394, 25);
+            this.Num2.MinimumSize = new System.Drawing.Size(100, 100);
+            this.Num2.Name = "Num2";
+            this.Num2.Size = new System.Drawing.Size(200, 100);
+            this.Num2.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(85, 176);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(122, 60);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(242, 175);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(107, 63);
+            this.btnSub.TabIndex = 3;
+            this.btnSub.Text = "__";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Location = new System.Drawing.Point(406, 173);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(107, 63);
+            this.btnMul.TabIndex = 4;
+            this.btnMul.Text = "*";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(552, 173);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(107, 63);
+            this.btnDiv.TabIndex = 5;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(108, 310);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(45, 16);
+            this.lblResult.TabIndex = 6;
+            this.lblResult.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.Num2);
+            this.Controls.Add(this.Num1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Num1;
+        private System.Windows.Forms.TextBox Num2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ WindowsFormsApp10/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(Num1.Text);
+            double num2 = Convert.ToDouble(Num2.Text);
+            double result = num1 - num2;
+            lblResult.Text = "Result: " + result;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(Num1.Text);
+            double num2 = Convert.ToDouble(Num2.Text);
+            double result = num1 + num2;
+            lblResult.Text = "Result: " + result;
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(Num1.Text);
+            double num2 = Convert.ToDouble(Num2.Text);
+            double result = num1 * num2;
+            lblResult.Text = "Result: " + result;
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(Num1.Text);
+            double num2 = Convert.ToDouble(Num2.Text);
+
+            if (num2 == 0)
+            {
+                MessageBox.Show("You cannot divide by zero!");
+            }
+            else
+            {
+                double result = num1 / num2;
+                lblResult.Text = "Result: " + result;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
